0d5ec618667c27b14eae311c5144dfb788b07acc
Maxb <maxb@jyzblm.com>
2016-12-21 12:59:11 +0800

    add databinding code

--- app/src/main/java/com/xiaobao/databindingdemo/MainActivity.java
+++ app/src/main/java/com/xiaobao/databindingdemo/MainActivity.java
@@ -1,13 +1,21 @@
 package com.xiaobao.databindingdemo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.xiaobao.databindingdemo.databinding.ActivityMainBinding;
+import com.xiaobao.databindingdemo.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        User u = new User("xiaobao");
+        binding.setUser(u);
+
     }
 }
